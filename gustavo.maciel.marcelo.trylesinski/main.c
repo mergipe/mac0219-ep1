8e36285b14771b626ef71641d1e0d5e4ca958d61
mergipe <gustavo.maciel@usp.br>
2018-05-10 11:50:49 -0300

    Novo argumento na linha de comando; mudança na mensagem de usage

--- gustavo.maciel.marcelo.trylesinski/main.c
+++ gustavo.maciel.marcelo.trylesinski/main.c
@@ -12,31 +12,35 @@ int main(int argc, char **argv)
     uint64_t m, p, n;
     FILE *aFile, *bFile, *cFile;
 
-    if (argc != 4)
+    if (argc != 5 || (argv[1][0] != 'o' && argv[1][0] != 'p'))
     {
-        printf("Usage: %s <path_to_A> <path_to_B> <path_to_C>\n", argv[0]);
+        printf("Usage: %s <implementation> <file_a> <file_b> <file_c>\n"
+               "  where <implementation> is\n"
+               "     p   for an implementation using pthreads\n"
+               "     o   for an implementation using OpenMP\n",
+               argv[0]);
         return EXIT_FAILURE;
     }
 
-    aFile = fopen(argv[1], "r");
+    aFile = fopen(argv[2], "r");
     if (aFile == NULL)
-    {
-        printf("Error opening file %s\n", argv[1]);
-        return EXIT_FAILURE;
-    }
-
-    bFile = fopen(argv[2], "r");
-    if (bFile == NULL)
     {
         printf("Error opening file %s\n", argv[2]);
         return EXIT_FAILURE;
     }
 
-    cFile = fopen(argv[3], "w");
+    bFile = fopen(argv[3], "r");
+    if (bFile == NULL)
+    {
+        printf("Error opening file %s\n", argv[3]);
+        return EXIT_FAILURE;
+    }
+
+    cFile = fopen(argv[4], "w");
 
     if (cFile == NULL)
     {
-        printf("Error opening file %s\n", argv[3]);
+        printf("Error opening file %s\n", argv[4]);
         return EXIT_FAILURE;
     }
 
