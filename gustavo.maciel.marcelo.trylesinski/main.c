6fd2032aca553920c2acafa3a87b38b4ce821ad1
mergipe <gustavo.maciel@usp.br>
2018-05-16 15:50:03 -0300

    Mensagens de erro em stderr em vez de stdout

--- gustavo.maciel.marcelo.trylesinski/main.c
+++ gustavo.maciel.marcelo.trylesinski/main.c
@@ -15,11 +15,12 @@ int main(int argc, char **argv)
 
     if (argc != 5 || (argv[1][0] != OPENMP_IMPL && argv[1][0] != PTHREAD_IMPL))
     {
-        printf("Usage: %s <implementation> <file_a> <file_b> <file_c>\n"
-               "  where <implementation> is\n"
-               "     p   for an implementation using pthreads\n"
-               "     o   for an implementation using OpenMP\n",
-               argv[0]);
+        fprintf(stderr,
+                "Usage: %s <implementation> <file_a> <file_b> <file_c>\n"
+                "  where <implementation> is\n"
+                "     p   for an implementation using pthreads\n"
+                "     o   for an implementation using OpenMP\n",
+                argv[0]);
         return EXIT_FAILURE;
     }
 
@@ -28,21 +29,21 @@ int main(int argc, char **argv)
     aFile = fopen(argv[2], "r");
     if (aFile == NULL)
     {
-        printf("Error opening file %s\n", argv[2]);
+        fprintf(stderr, "Error opening file %s\n", argv[2]);
         return EXIT_FAILURE;
     }
 
     bFile = fopen(argv[3], "r");
     if (bFile == NULL)
     {
-        printf("Error opening file %s\n", argv[3]);
+        fprintf(stderr, "Error opening file %s\n", argv[3]);
         return EXIT_FAILURE;
     }
 
     cFile = fopen(argv[4], "w");
     if (cFile == NULL)
     {
-        printf("Error opening file %s\n", argv[4]);
+        fprintf(stderr, "Error opening file %s\n", argv[4]);
         return EXIT_FAILURE;
     }
 
