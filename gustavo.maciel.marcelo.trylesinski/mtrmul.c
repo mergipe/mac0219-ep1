0391dbb5d3ed41bf8a7da17c4651449461552045
mergipe <gustavo.maciel@usp.br>
2018-05-17 17:07:00 -0300

    Passo 1 da otimização

--- gustavo.maciel.marcelo.trylesinski/mtrmul.c
+++ gustavo.maciel.marcelo.trylesinski/mtrmul.c
@@ -5,6 +5,15 @@
 #define A(i,j) a[j * lda + i]
 #define B(i,j) b[j * ldb + i]
 #define C(i,j) c[j * ldc + i]
+#define a(k)   a[k * lda]
+#define b(k)   b[k]
+
+/*  */
+static void dotprod(double *a, uint64_t lda, double *b, double *c, uint64_t p)
+{
+    for (uint64_t k = 0; k < p; k++)
+        *c += a(k) * b(k);
+}
 
 void mtrmul_naive(uint64_t m, uint64_t p, uint64_t n,
                   double *a, uint64_t lda,
@@ -25,6 +34,6 @@ void mtrmul_opt(uint64_t m, uint64_t p, uint64_t n,
     for (uint64_t j = 0; j < n; j++)
         for (uint64_t i = 0; i < m; i++)
             for (uint64_t k = 0; k < p; k++)
-                C(i,j) = C(i,j) + A(i,k) * B(k,j);
+                dotprod(&A(i,0), lda, &B(0,j), &C(i,j), p);
 
 }
